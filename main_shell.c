243707eb21fd8d0724878fe3418f0d1d8dd7d885
desigveloper <habibumamudu@gmail.com>
2023-07-14 03:38:55 -0700

    junk simple program for test

---
+++ main_shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char name[20];
+
+	printf("Enter the project name here: \n");
+	scanf("%s", name);
+
+	printf("The project is %s\n", name);
+
+	return (0);
+}
