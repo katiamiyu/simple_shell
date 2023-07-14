bbf3a753b0665c2498c4d8110bbd49f84c4bf021
katiamiyu <katiamiyu@gmail.com>
2023-07-14 10:56:51 +0000

    shell prompt

--- main_shell.c
+++ main_shell.c
@@ -4,10 +4,12 @@ int main(void)
 {
 	char name[20];
 
-	printf("Enter the project name here: \n");
-	scanf("%s", name);
+	do {
+		printf("$ ");
+		scanf("%s", name);
 
-	printf("The project is %s\n", name);
+		printf("command not available yet %s\n", name);
+	} while (1);
 
 	return (0);
 }
