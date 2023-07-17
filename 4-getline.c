7bd1dacd760889f7d1d4c1e9fc99ecfe386282fa
desigveloper <habibumamudu@gmail.com>
2023-07-17 01:32:44 -0700

    update on getline

--- 4-getline.c
+++ 4-getline.c
@@ -11,29 +11,32 @@
 
 #include "main_shell.h"
 
-ssize_t _getline(char **ptr_buff, size_t *ptr_size, FILE *input_stream)
+ssize_t  _getline(char **ptr_buff, size_t *ptr_size, FILE *input_stream)
 {
-	ssize_t buffer = *ptr_size;
-	ssize_t len = 0;
+	ssize_t buffer,  len;
 	int ch;
 	char *new_buffer;
 
 	if (ptr_buff == NULL || ptr_size == NULL)
 		return (-1);
 
-	while ((ch = fgetc(input_stream)) != '\0')
+	buffer = *ptr_size;
+	len = 0;
+
+	while ((ch = fgetc(input_stream)) != EOF)
 	{
+		/* Checks if line length is lager than allocated buffer size */
 		if (len >= buffer - 1)
 		{
 			buffer *= 2;
 
-			/* resizes the buffer by doubling it */
-			/* if is the buffer is to small to hold entire line */
+			/* Resizes the buffer by doubling it */
+			/* if it's too small to hold entire line */
 			new_buffer = realloc(*ptr_buff, buffer);
 			if (new_buffer == NULL)
 				return (-1);
 
-			(*ptr_buff) = new_buffer;
+			*ptr_buff = new_buffer;
 			*ptr_size = buffer;
 		}
 
@@ -45,7 +48,7 @@ ssize_t _getline(char **ptr_buff, size_t *ptr_size, FILE *input_stream)
 
 	(*ptr_buff)[len] = '\0';
 
-	if (len == 0 && ch == '\0')
+	if (len == 0 && ch == EOF)
 		return (-1);
 
 	return (len);
--- main_shell.h
+++ main_shell.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 ssize_t _getline(char **ptr_buff, size_t *ptr_size, FILE *input_stream);
-
 #endif
